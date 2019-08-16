06d6aec619c87bf8437604841afc450da2678800
pavithramani1806 <47516322+pavithramani1806@users.noreply.github.com>
2019-08-16 22:21:49 +0530

    Update q31.c

--- guvi/c/begin/set4/q31.c
+++ guvi/c/begin/set4/q31.c
@@ -1,1 +1,29 @@
-
+#include <stdio.h>
+ 
+int main()
+{
+    char    str[100];
+    int countDigits,countAlphabet,countSpecialChar,countSpaces;
+    int counter;
+ 
+    //assign all counters to zero
+    countDigits=countAlphabet=countSpecialChar=countSpaces=0;
+ 
+    printf("Enter a string: ");
+    gets(str);
+ 
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+ 
+        if(str[counter]>='0' && str[counter]<='9')
+            countDigits++;
+        else if((str[counter]>='A' && str[counter]<='Z')||(str[counter]>='a' && str[counter]<='z'))
+            countAlphabet++;
+        else
+            countSpecialChar++;
+    }
+ 
+    printf(" \nAlphabets: %d",countAlphabet);
+ 
+    return 0;
+}
